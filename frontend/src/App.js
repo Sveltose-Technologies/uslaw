8c2f7ba90f5aeb75df5c7a52b639e0edb57923ac
anujeshyadav <sveltosehr@gmail.com>
2026-03-10 13:09:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Happiness
         </a>
       </header>
     </div>
